dfbf13c259286fa3cfc67961e330f3f3819cb60c
krishtheavenge <75679477+krishtheavenge@users.noreply.github.com>
2020-12-18 17:58:43 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,15 @@
-  
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+var firebaseConfig = {
+    apiKey: "AIzaSyC23G8IGsPRB5bJPxFBiXLVj4hDDi3WdOM",
+    authDomain: "project-kwitter-e829d.firebaseapp.com",
+    projectId: "project-kwitter-e829d",
+    storageBucket: "project-kwitter-e829d.appspot.com",
+    messagingSenderId: "963106151404",
+    appId: "1:963106151404:web:997b0c3984f09054ddbec9",
+    measurementId: "G-DN434SHNFN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
